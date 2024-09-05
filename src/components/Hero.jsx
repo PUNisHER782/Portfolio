87d2ef4138743d675d7e55e12fb1b713f9c0ee0e
PuN|$hER <143728831+PUNisHER782@users.noreply.github.com>
2024-09-05 23:23:31 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,8 @@
-import { motion } from "framer-motion";
-
-import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
-
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        classNae={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -26,7 +21,9 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
-
+      
+// import { ComputersCanvas } from "./canvas";
+      
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
@@ -49,3 +46,7 @@ const Hero = () => {
 };
 
 export default Hero;
+
+// import { motion } from "framer-motion";
+
+// import { styles } from "../styles";
